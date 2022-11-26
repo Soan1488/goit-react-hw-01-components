c57a23d6f42d1475bd83050a5206560e87f8c9ed
Your NameSoan1488 <andrisokolovskiy@gmail.com>
2022-11-26 01:55:43 +0100

    task 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-import {Profile} from './Profile/Profile'
-import user from './Profile/user.json'
+import Profile from './Profile/Profile';
+import Section from './Container/Container';
+import user from './Profile/user.json';
 
-export const App = () => {
+export default function App() {
   return (
-    <div>
-      <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-    </div>
+    <Section>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+    </Section>
   );
-};
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+export default function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
